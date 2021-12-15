5878f758448cf8987c94e72da6713480df517afd
Markelpollo <93318690+Markelpollo@users.noreply.github.com>
2021-12-15 10:35:27 +0100

    Add files via upload

---
+++ python4.py
@@ -0,0 +1,5 @@
+﻿entrada_usuario = (input)("Una cereza es A o B:")
+if entrada_usuario.upper() == "A":
+    print("Correcto, la guinda del postre!")
+else:
+    print("Incorrecto, la guinda del desierto es incorrecto")
